73388b35dd1addcdc597111bad65b73838729844
Ridoy Chandra Dey <iamridoydey@gmail.com>
2024-09-30 20:41:21 +0600

    Navbar, sidebar, homepage and parallax completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import "./app.scss"
+import "./app.scss";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
 
 const App = () => {
-  return <div>
-    <section>Hero</section>
-    <section>Paralax</section>
-    <section>Services</section>
-    <section>Parallax</section>
-    <section>Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
-    <section>Contact</section>
-  </div>;
+  return (
+    <div>
+      <section id="homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="services">
+        <Parallax type="services" />
+      </section>
+      <section>Services</section>
+      <section id="portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section id="about">Contact</section>
+    </div>
+  );
 };
 
 export default App;
