ea6f1e0d5c034d3d483717c4d82485638a240858
Ridoy Chandra Dey <iamridoydey@gmail.com>
2024-10-03 09:54:20 +0600

    Portfolio section and contanct section done excluding form submission

--- src/components/portfolio/Item.jsx
+++ src/components/portfolio/Item.jsx
@@ -1,9 +1,37 @@
-const Item = ({item}) => {
+import { useEffect, useRef } from "react";
+import "./item.scss";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+const Item = ({ item }) => {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    // offset: ["start start", "end start"],
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-100, 100]);
+
+  useEffect(() => {
+    const unsubscribe = scrollYProgress.onChange((latest) => {
+      console.log("Scroll Progress:", latest);
+    });
+
+    // Cleanup function to unsubscribe from the motion value
+    return () => unsubscribe();
+  }, [scrollYProgress]); // Dependency array ensures effect runs when scrollYProgress changes
+
   return (
-    <div className="portfolio_itme">
-      <section>{item.title}</section>
-    </div>
-  )
-}
+    <section className="portfolio_item">
+      <div className="img_container" ref={ref}>
+        <img src={item.img} alt={`${item.title} picture`} />
+      </div>
+      <motion.div className="text_container" style={{ y }}>
+        <h1 className="text_title">{item.title}</h1>
+        <p className="text_para">{item.description}</p>
+        <button className="text_button">See Demo</button>
+      </motion.div>
+    </section>
+  );
+};
 
 export default Item;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,30 +7,30 @@ const Portfolio = () => {
   const items = [
     {
       id: 1,
-      title: "Amazonia",
-      img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdev.to%2Fbasir%2Freact-ecommerce-project-2021-in-9-hours-21n7&psig=AOvVaw3IGzintOJkLctD6ZGxgyWH&ust=1727860499639000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMDZwYzs7IgDFQAAAAAdAAAAABAK",
-      descript:
+      title: "Amazona",
+      img: "/amazona.jpg",
+      description:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam voluptatem temporibus assumenda illo laboriosam reprehenderit repellat alias ratione eum deleniti. Ipsam, aliquam ea. Inventore, sapiente.",
     },
     {
       id: 2,
       title: "Banking App",
-      img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dhiwise.com%2Ftemplates%2Ffirst-bank-digital-banking-react&psig=AOvVaw1dQ9BAWCzg7HP2W7y69KP7&ust=1727860710294000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJD_2_Ds7IgDFQAAAAAdAAAAABAE",
-      descript:
+      img: "/banking.jpg",
+      description:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam voluptatem temporibus assumenda illo laboriosam reprehenderit repellat alias ratione eum deleniti. Ipsam, aliquam ea. Inventore, sapiente.",
     },
     {
       id: 3,
       title: "Restaurant App",
-      img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Freactjsexample.com%2F10-best-react-restaurant-website-templates%2F&psig=AOvVaw0F3U103muEeeJRINEwtrQ1&ust=1727860748204000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKi91obt7IgDFQAAAAAdAAAAABAJ",
-      descript:
+      img: "/restaurant.webp",
+      description:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam voluptatem temporibus assumenda illo laboriosam reprehenderit repellat alias ratione eum deleniti. Ipsam, aliquam ea. Inventore, sapiente.",
     },
     {
       id: 4,
       title: "Music App",
-      img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Ffreefrontend.com%2Freact-music-players%2F&psig=AOvVaw1We6c0tDK-4V_fzOpE1Nng&ust=1727860791362000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPCzyJft7IgDFQAAAAAdAAAAABAE",
-      descript:
+      img: "/music.png",
+      description:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam voluptatem temporibus assumenda illo laboriosam reprehenderit repellat alias ratione eum deleniti. Ipsam, aliquam ea. Inventore, sapiente.",
     },
   ];
@@ -49,9 +49,11 @@ const Portfolio = () => {
         <h1 className="progress_bar_heading">Feature Works</h1>
         <motion.div style={{ scaleX }} className="progress_bar"></motion.div>
       </div>
-      {items.map((item, i) => (
+      <div className="items">
+        {items.map((item, i) => (
         <Item item={item} key={i} />
       ))}
+      </div>
     </div>
   );
 };
